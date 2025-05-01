94a58e6dd2955ea6725e616ff4b700acc33550b9
Benjamin Farthing <benjamin.farthing@gmail.com>
2025-04-30 21:38:14 -0400

    fixed the assignment to account for any flavor put into the console :)

--- index.js
+++ index.js
@@ -4,56 +4,20 @@ let input = prompt(
   "enter flavors here"
 );
 
-//splits each flavor into an array
-const flavors = input.split(",");
+//splits each flavor into an array and trim spaces
+const flavors = input.split(",").map((flavor) => flavor.trim().toLowerCase());
 
-//functions to total each ordered flavor (added an or statement to include if user adds a space between flavors)
-function vanillaTotal(flavors) {
-  let sum = 0;
-  for (let i = 0; i < flavors.length; i++)
-    if (flavors[i] == "vanilla" || flavors[i] == " vanilla") {
-      sum += 1;
-    } else sum += 0;
-  return sum;
+// make an object to track flavor counts
+const flavorCounts = {};
+
+for (const flavor of flavors) {
+  //if flavor already exists, increment count
+  if (flavorCounts[flavor]) {
+    flavorCounts[flavor]++;
+  } else {
+    flavorCounts[flavor] = 1;
+  }
 }
-function strawberryTotal(flavors) {
-  let sum = 0;
-  for (let i = 0; i < flavors.length; i++)
-    if (flavors[i] == "strawberry" || flavors[i] == " strawberry") {
-      sum += 1;
-    } else sum += 0;
-  return sum;
-}
-function coffeeTotal(flavors) {
-  let sum = 0;
-  for (let i = 0; i < flavors.length; i++)
-    if (flavors[i] == "coffee" || flavors[i] == " coffee") {
-      sum += 1;
-    } else sum += 0;
-  return sum;
-}
-//added to test my knowledge
-function otherTotal(flavors) {
-  let sum = 0;
-  for (let i = 0; i < flavors.length; i++)
-    if (
-      flavors[i] != "coffee" &&
-      flavors[i] != " coffee" &&
-      flavors[i] != "vanilla" &&
-      flavors[i] != " vanilla" &&
-      flavors[i] != "strawberry" &&
-      flavors[i] != " strawberry"
-    ) {
-      sum += 1;
-    } else sum += 0;
-  return sum;
-}
-//object to arrange the totals of each flavor ordered
-const flavorsTotal = {
-  vanilla: vanillaTotal(flavors),
-  strawberry: strawberryTotal(flavors),
-  coffee: coffeeTotal(flavors),
-  other: otherTotal(flavors),
-};
-//log to display totals in the console
-console.log(flavorsTotal);
+
+//Output the final tally
+console.log(flavorCounts);
